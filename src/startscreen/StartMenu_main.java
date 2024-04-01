bea3db10d5869674220fd9379e821ddbf296448d
HuisuLim <dlagmltn0217@gmail.com>
2024-04-01 23:58:07 +0900

    .

--- src/startscreen/StartMenu_main.java
+++ src/startscreen/StartMenu_main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 
 public class StartMenu_main {
+    //
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
