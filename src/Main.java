964a1a60167a5d9b02f74f76a99cd52663a25e42
HuisuLim <dlagmltn0217@gmail.com>
2024-05-08 19:56:23 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             public void run() {
                 StartMenu menu = new StartMenu();
                 menu.setVisible(true);
-                //fff
+                //fffddd
             }
         });
     }
