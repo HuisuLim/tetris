557d006ac2af25924d6dde09439443463313f58b
이용주 <89644553+yongzooda@users.noreply.github.com>
2024-05-10 11:43:34 +0900

    Update StartMenuMain.java

--- src/startscreen/StartMenuMain.java
+++ src/startscreen/StartMenuMain.java
@@ -5,14 +5,15 @@ import javax.swing.*;
 public class StartMenuMain {
     public static void main(String[] args) {
         try {
+            // 시스템 기본 룩앤필 설정
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // 이벤트 디스패치 스레드에서 UI를 시작합니다.
         SwingUtilities.invokeLater(() -> {
-            StartMenuController menuController = new StartMenuController();
-            menuController.getView().setVisible(true); // 컨트롤러를 통해 뷰를 얻어 표시
+            new StartMenuView(); // 컨트롤러가 생성될 때 뷰도 함께 생성되고 설정됩니다.
         });
     }
 }
