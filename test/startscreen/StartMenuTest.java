1398d3e1973e0b16bd393765fd6f35bd313eff98
HuisuLim <dlagmltn0217@gmail.com>
2024-05-21 21:32:51 +0900

    StartMenuTest.java testDefaultSettings method를 위한 BeforeEach, AfterEach추가

--- test/startscreen/StartMenuTest.java
+++ test/startscreen/StartMenuTest.java
@@ -3,15 +3,26 @@ package startscreen;
 import javax.swing.*;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import settings.settingModel;
 
 import java.awt.*;
+import java.io.File;
 
 public class StartMenuTest {
 
     private StartMenu startMenu;
+    private static final String TEST_SETTINGS_FILE = "test/settings/settings.properties";
+    private settingModel model;
+
 
     @BeforeEach
     public void setUp() {
+        File file = new File(TEST_SETTINGS_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
+        model = new settingModel();
+        model.setSettingsFile(TEST_SETTINGS_FILE);
         try {
             SwingUtilities.invokeAndWait(() -> startMenu = new StartMenu());
         } catch (Exception e) {
@@ -19,9 +30,10 @@ public class StartMenuTest {
         }
     }
 
+
+
     @Test
     public void testDefaultSettings() {
-
         assertEquals(1.6, StartMenu.screenRatio, 0.01);
         assertFalse(StartMenu.isColorblindness);
         assertEquals("ArrowKeys", StartMenu.keySetting);
@@ -38,5 +50,9 @@ public class StartMenuTest {
     @AfterEach
     public void tearDown() {
         startMenu.dispose();
+        File file = new File(TEST_SETTINGS_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
     }
 }
